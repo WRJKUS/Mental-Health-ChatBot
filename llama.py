2f780fe7f2d6a477a04969c13ca259cf7beccf56
WRJKUS <95240506+WRJKUS@users.noreply.github.com>
2023-08-26 17:12:12 +0200

    get_response

--- llama.py
+++ llama.py
@@ -28,37 +28,38 @@ stub = service_pb2_grpc.V2Stub(channel)
 metadata = (('authorization', 'Key ' + PAT),)
 
 userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
-
-post_workflow_results_response = stub.PostWorkflowResults(
-    service_pb2.PostWorkflowResultsRequest(
-        user_app_id=userDataObject,
-        workflow_id=WORKFLOW_ID,
-        inputs=[
-            resources_pb2.Input(
-                data=resources_pb2.Data(
-                    text=resources_pb2.Text(
-                        url=TEXT_FILE_URL
+def get_response( user_prompt):
+    post_workflow_results_response = stub.PostWorkflowResults(
+        service_pb2.PostWorkflowResultsRequest(
+            user_app_id=userDataObject,
+            workflow_id=WORKFLOW_ID,
+            inputs=[
+                resources_pb2.Input(
+                    data=resources_pb2.Data(
+                        text=resources_pb2.Text(
+                            raw=user_prompt
+                        )
                     )
                 )
-            )
-        ]
-    ),
-    metadata=metadata
-)
-if post_workflow_results_response.status.code != status_code_pb2.SUCCESS:
-    print(post_workflow_results_response.status)
-    raise Exception("Post workflow results failed, status: " + post_workflow_results_response.status.description)
-
+            ]
+        ),
+        metadata=metadata
+    )
+    if post_workflow_results_response.status.code != status_code_pb2.SUCCESS:
+        print(post_workflow_results_response.status)
+        raise Exception("Post workflow results failed, status: " + post_workflow_results_response.status.description)
+    results = post_workflow_results_response.results[0]
+    return results
 # We'll get one WorkflowResult for each input we used above. Because of one input, we have here one WorkflowResult
-results = post_workflow_results_response.results[0]
+
 
 # Each model we have in the workflow will produce one output.
-for output in results.outputs:
-    model = output.model
+# ##for output in results.outputs:
+#    model = output.model
 
-    print("Predicted concepts for the model `%s`" % model.id)
-    for concept in output.data.concepts:
-        print("	%s %.2f" % (concept.name, concept.value))
+#    print("Predicted concepts for the model `%s`" % model.id)
+#   for concept in output.data.concepts:
+#        print("	%s %.2f" % (concept.name, concept.value))
 
 # Uncomment this line to print the full Response JSON
 #print(results)
